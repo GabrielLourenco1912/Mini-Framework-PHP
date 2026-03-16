0ed7ab23ebfb88fce9ca182193f9d3e34f7e2dba
gabriel <gabri02gomes@hotmail.com>
2026-03-16 01:19:56 -0300

    middlewares e weblogin

--- config/routes/web.php
+++ config/routes/web.php
@@ -9,24 +9,22 @@ use App\Controllers\BDController;
 /** @var \App\Core\Router $router */
 
 $router->group(['middleware' => 'guest'], function($r) {;
-    $r->get('/auth/register', [AuthController::class, 'register']);
-    $r->get('/auth/login', [AuthController::class, 'login']);
+    $r->get('/auth/login', [AuthController::class, 'index']);
+    $r->post('/auth/login', [AuthController::class, 'login']);
 });
-
-$router->delete('/bd/schemaDelete', [BDController::class, 'schemaDelete']);
 
 $router->get('/', [HomeController::class, 'index']);
 
-$router->get('/bd/schemaBuilder', [BDController::class, 'schemaBuilder']);
+$router->group(['middleware' => 'admin'], function($r) {
+    $r->get('/bd/schemaBuilder', [BDController::class, 'schemaBuilder']);
 
-$router->get('/bd/schemaCreate', [BDController::class, 'schemaCreate']);
+    $r->get('/bd/schemaCreate', [BDController::class, 'schemaCreate']);
 
-$router->get('/bd/schemaView', [BDController::class, 'schemaView']);
+    $r->get('/bd/schemaView', [BDController::class, 'schemaView']);
 
-$router->post('/bd/schemaBuilder', [BDController::class, 'schemaBuilderGenerate']);
+    $r->post('/bd/schemaBuilder', [BDController::class, 'schemaBuilderGenerate']);
 
-$router->get('/bd/schemaCreate/success', [BDController::class, 'schemaCreateSuccess']);
+    $r->get('/bd/schemaCreate/success', [BDController::class, 'schemaCreateSuccess']);
 
-$router->group(['middleware' => 'auth'], function($r) {
-    $r->get('/dashboard', [HomeController::class, 'dashboard']);
+    $r->delete('/bd/schemaDelete', [BDController::class, 'schemaDelete']);
 });
